be6a567ed6e845af2bf1ad16dce6582c8eed9d63
letakkak32 <letakkak32@naver.com>
2023-01-29 17:01:21 +0900

    Initial commit

---
+++ src/main/java/letakkak32/betterban/PlMain.java
@@ -0,0 +1,17 @@
+package letakkak32.betterban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic    
+    }
+}
