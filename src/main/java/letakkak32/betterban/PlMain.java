fd0c0c8856f0a87967d9bb0dd73b60944e31e667
letakkak32 <letakkak32@naver.com>
2023-01-29 20:07:48 +0900

    저장

--- src/main/java/letakkak32/betterban/PlMain.java
+++ src/main/java/letakkak32/betterban/PlMain.java
@@ -1,5 +1,8 @@
 package letakkak32.betterban;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlMain extends JavaPlugin {
@@ -7,11 +10,25 @@ public final class PlMain extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getLogger().info("letakkak32's better ban plugin is enabled!");
+    }
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info("letakkak32's better ban plugin is disabled... See you again!");
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic    
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        switch (label)
+        {
+            case "ban":
+            {
+                if(sender.isOp())
+                {
+
+                }
+            }
+        }
+        return true;
     }
 }
