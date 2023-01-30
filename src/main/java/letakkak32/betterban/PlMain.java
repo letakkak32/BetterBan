8a9a02db19c2d0ef5f52596867458cf4cfaa2168
letakkak32 <letakkak32@naver.com>
2023-01-30 10:16:37 +0900

    저장

--- src/main/java/letakkak32/betterban/PlMain.java
+++ src/main/java/letakkak32/betterban/PlMain.java
@@ -1,5 +1,6 @@
 package letakkak32.betterban;
 
+import org.bukkit.BanList;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -31,11 +32,14 @@ public final class PlMain extends JavaPlugin { //testing version please bulid th
                         Player target = Bukkit.getPlayer(args[0]);
                         if(target != null)
                         {
-                            String targetsIp = String.valueOf(target.getAddress());
+                            String targetsIp = String.valueOf(target.getAddress()); //todo
                             //이제야 밴할 수 있는 관문을 통과함! 나이스하다
-                        }
-                    }
-                }
+                            /*This is test code!*/
+                            sender.sendMessage(target.getName() + targetsIp); //테스트. 타켓의 이름과 아이피를 출력.
+
+                        } else {sender.sendMessage("Oh... This player is not in server!");} //입력한 플레이어가 서버에 존재하지 않음
+                    } else {sender.sendMessage("Oh... This is wrong command! usage: /ban [playername]");} //이 args.length 1이 아님
+                } else {sender.sendMessage("Oh... You don't have permission... You can't use this command!");} //OP가 아님
             }
         }
         return true;
